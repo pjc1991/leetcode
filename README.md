2fb69c764efd27fd5c311a653b676acc1d788754
pjc1991 <whdekf@naver.com>
2021-04-18 19:36:53 +0900

    time to leetcode

---
+++ README.md
@@ -0,0 +1,7 @@
+## leetcode learning
+
+As a back-end programmer, I need to learn some algorithms to optimize my logic and codes. 
+
+As a programmer, you would know that people will forget everything if they don't write down somewhere.
+
+that's why this repository exists.
---
+++ src/ReverseString.java
@@ -0,0 +1,29 @@
+public class ReverseString {
+    public static void main(String[] args) {
+
+        // simple test case 
+        char [] s = {'h','e','l','l','o'};
+        Solution solution = new Solution();
+        solution.reverseString(s);
+        
+        System.out.println(s);
+
+
+    }
+}
+
+class Solution {
+    public void reverseString(char[] s) {
+        int length = s.length;
+
+        // only loop half of the array
+        for (int i = 0;i<length/2;i++){
+            // swap front and back chars
+            char front = s[i];
+            // be careful that the last index is the length -1
+            char back = s[length - 1 - i];
+            s[i] = back;
+            s[length - 1 - i] = front;
+        }
+    }
+}
