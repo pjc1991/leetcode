83ba181978f67a52fec753f73573aaadbb9341f3
pjc1991 <whdekf@naver.com>
2021-04-19 22:02:45 +0900

    FirstBadVersion solution.

---
+++ src/FirstBadVersion.java
@@ -0,0 +1,41 @@
+
+public class FirstBadVersion {
+    public static void main(String[] args) {
+
+        int badVersion = 1702766719;
+        int n = 2126753390;
+        SolutionFirstBadVersion solution = new SolutionFirstBadVersion();
+        solution.badVersion = badVersion;
+
+        int answer = solution.firstBadVersion(n);
+        System.out.println("Input { Bad Version : " + badVersion + ", N : " + n + " } ");
+        System.out.println("Answer { Bad Version : " + answer + " } ");
+    }
+}
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+class VersionControl{
+    int badVersion = 0;
+    public boolean isBadVersion(int version){
+        return (version >= this.badVersion);
+    }
+    
+    
+}
+class SolutionFirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        long begin = 0;
+        long end = n;
+        while(begin < end){
+            boolean downward = isBadVersion((int) ((begin + end) / 2));
+            if (downward){
+                end = (begin + end) / 2;
+            } else {
+                begin = (begin + end) / 2 + 1;
+            }
+
+        }
+        return (int) begin;
+    }
+}
